23486be7d23e8f8e839bc1cca2bfa75bcccea2c0
piton13 <igor13luk@gmail.com>
2015-04-28 18:24:24 +0300

    [Project start] Place old html markup into markup

--- gulp/config.js
+++ gulp/config.js
@@ -21,7 +21,11 @@ var paths = new Layout({
         html: {
             _root: '/',
             pages: '',
-            components: 'html'
+            components: 'html',
+            markup: {
+                _root: 'html',
+                all_pages: ['project-work.html', 'today.html', 'week.html']
+            }
         }
     },
     dist: {
@@ -29,7 +33,8 @@ var paths = new Layout({
         html: {
             _root: '/',
             pages: '',
-            views: 'views'
+            views: 'views',
+            markup: 'markup'
         },
         js: 'js',
         css: 'css',
@@ -53,7 +58,7 @@ var patterns = new Layout({
             _root: '/',
             pages: glob.allOfType('html'),
             pageComponents: glob.deepAllOfType('html').build('html'),
-            views: glob.deepAllOfType('html').build('js')
+            views: glob.deepAllOfType('html').build('js'),
         },
         js: glob.deepAllOfType('js').build('js'),
         scss: glob.deepAllOfType('scss').build('scss'),
@@ -72,7 +77,8 @@ var patterns = new Layout({
         html: {
             _root: '/',
             pages: glob.allOfType(),
-            views: glob.deepAllOfType().build('views')
+            views: glob.deepAllOfType().build('views'),
+            markup: glob.deepAllOfType('html').build('markup')
         },
         js: glob.deepAllOfType().build('js'),
         css: glob.deepAllOfType().build('css'),
