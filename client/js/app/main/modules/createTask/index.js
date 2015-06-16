b536456413067ca235bcc37fe1bd62d26e251077
Borys Ihnatyev <paur.link@gmail.com>
2015-06-17 02:14:49 +0300

    [Form Header][Project edition] - refactored form header

--- client/js/app/main/modules/createTask/index.js
+++ client/js/app/main/modules/createTask/index.js
@@ -1,7 +1,6 @@
 module.exports = require('angular')
   .module('createTaskModule', [
-    require('./validateTask'),
+    require('./validateTask')
   ])
-  .directive('dwTaskHeader', require('./header/taskHeader.directive'))
   .config(require('./createTask.config.js'))
   .name;
